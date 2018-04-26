511c03f7300ec0d79f2feb71a9d561c8c4d539f3
smithwebtek <brad@smithwebtek.com>
2018-04-26 00:44:00 -0400

    Add cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 		origins 
 		'http://localhost:3000',
 		'https://localhost:3000',
-		'https://127.0.0.1:3000',
+		'https://127.0.0.1:3000'
 
     resource '*',
       headers: :any,
