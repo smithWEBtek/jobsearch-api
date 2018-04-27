00d6ece82938ca1af62ed1c237b42a6ed5aadad4
smithwebtek <brad@smithwebtek.com>
2018-04-27 00:45:20 -0400

    Add url to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-		origins 'http://localhost:3000', 'https://localhost:3000', 'https://127.0.0.1:3000'
+		origins 'http://localhost:3000', 'https://localhost:3000', 'https://127.0.0.1:3000', 'http://127.0.0.1:3000' 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
